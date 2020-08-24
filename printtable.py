3b6aa4df363c4b08df15d9af5d4e610a7aef1a2d
Olav Vahtras <vahtras@kth.se>
2020-08-24 17:41:35 +0200

    Introduce multtable function

--- printtable.py
+++ printtable.py
@@ -1,2 +1,9 @@
-for i in range(1, 5):
-    print(i*7)
+def multtable(start, stop, number):
+    """
+    Print multiplication table for <number>
+    from <start> to including <stop> 
+    """
+    for i in range(start, stop+1):
+        print(f"{i} x {number} = {i*number}")
+
+multtable(1, 4, 7)
