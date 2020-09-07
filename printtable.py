a3330e41697df916ec4df0edd600bf4e256fe5c4
nikivon <vontzou@kth.se>
2020-09-07 21:47:18 +0200

    modified

--- printtable.py
+++ printtable.py
@@ -1,10 +1,9 @@
-def multtable(start, stop, number):
-    """
-    Print multiplication table for <number>
-    from <start> to including <stop> 
-    """
-    for i in range(start, stop+1):
-        print(f"{i} x {number} = {i*number}")
-
+def powertable(power, number):
+    '''
+    prints power table for number in the power of "power"
+    '''
+    for i in range(1, number + 1):
+        print(i ** power)
 if __name__ == '__main__':
-    multtable(1, 4, 7)
+    powertable(2,4)
+    powertable(3,3)
